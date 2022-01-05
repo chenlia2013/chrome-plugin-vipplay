826c8195045532bf5f913ae2b83196e049aa6d42
chenlia2013 <506766132@qq.com>
2022-01-05 12:43:16 +0800

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,9 @@
+﻿// 新标签打开网页
+$('#open_url_new_tab').click(() => {
+	chrome.tabs.getSelected(null, function(tab)
+	{
+		chrome.tabs.create({url: 'https://www.8090.la/8090/?url=' + tab.url});
+	});
+});
+
+
